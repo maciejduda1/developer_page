88207292d82c23d70bb48f42c0358e953cdfb924
Maciej Duda <maciej.duda1@gazeta.pl>
2024-08-08 10:29:17 +0200

    about done rwd

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
 		extend: {
+			colors: {
+				primary: "#070F2B",
+				secondary: "#1B1A55",
+				accent: "#535C91",
+			},
 			backgroundImage: {
 				"hero-phone": "url('~/images/nexus5.svg')",
 				"hero-pc": "url('~/images/monitor.svg')",
